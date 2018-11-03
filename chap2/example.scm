b0adaf5e5e81bea95de94f120f81af41905b2d50
liuweibo <liuweibo@360.cn>
2018-11-03 17:26:00 +0800

    [add] add example.scm

---
+++ chap2/example.scm
@@ -0,0 +1,8 @@
+; representation of the rational numbers
+; closure to represent special data
+; symbolic differentiation, the representation of sets, the encoding of information
+; generic operations, In particular, we introduce data-directed programming as a technique that allows individual data representations to be designed in isolation and then combined additively
+; polynomials' operations
+
+; 2.1 introduction to data abstraction "concrete data" <=== selector, consturctor ===> "abstract data"
+;
