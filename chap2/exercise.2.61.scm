4d260e01cafea94d989fddb969297719e3b7f5d0
NetWilliam <NWm.LWB@gmail.com>
2018-11-21 21:39:23 +0800

    [add] exercise 2.61 and 2.62 done

--- chap2/exercise.2.61.scm
+++ chap2/exercise.2.61.scm
@@ -2,16 +2,12 @@
 
 ; 2.6.1 and 2.6.2
 (define (adjoin-set x set)
-  (if ))
+  (cond ((null? set) (list x))
+        ((< x (car set)) (cons x set))
+        ((= x (car set)) set)
+        (else (cons (car set) (adjoin-set x (cdr set))))))
 
 (define (union-set set1 set2)
   (if (or (null? set1))
       set2
-      (let ((x1 (car set1)) (x2 (car set2)))
-        (cond ((= x1 x2)
-               (cons x1 (union-set (cdr set1)
-                                   (cdr set2))))
-              ((< x1 x2)
-               (union-set (cdr set1) set2))
-              ((< x2 x1)
-               (union-set set1 (cdr set2)))))))
+    (union-set (cdr set1) (adjoin-set (car set1) set2))))
