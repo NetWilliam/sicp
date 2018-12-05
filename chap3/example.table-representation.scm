2d677b13f721909c5ef49fbc8e6425d3f64b2d5a
NetWilliam <NWm.LWB@gmail.com>
2018-12-05 23:12:09 +0800

    [mod] mod examples

--- chap3/example.table-representation.scm
+++ chap3/example.table-representation.scm
@@ -4,8 +4,20 @@
   (let ((record (assoc key (cdr table))))
     (if record
         (cdr record)
-        #f)))
+      #f)))
 (define (assoc key records)
   (cond ((null? records) #f)
         ((equal? key (caar records)) (car records))
         (else (assoc key (cdr records)))))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+      (set-cdr! table
+                (cons (cons key value)
+                      (cdr table)))))
+  'ok)
+
+(define (make-table)
+  (list '*table*))
