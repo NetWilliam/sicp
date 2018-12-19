4c1a7652400baf8500608b0710d559d14447afd3
NetWilliam <NWm.LWB@gmail.com>
2018-12-19 08:57:30 +0800

    [mod] more explore in exercise.4.5.scm

--- chap4/exercise.4.5.scm
+++ chap4/exercise.4.5.scm
@@ -10,7 +10,8 @@
 (define (cond-clause-with-extension? clause)
   (memq '=> clause))
 (define (cond-action-with-extension clause) (cddr clause))
-(define (cond-predicate-with-extenssion clause) (car clause))
+(define (cond-predicate-with-extension clause) (car clause))
+(define (cond-recipient-with-extension clause) (caddr clause))
 
 (define (expand-clauses clauses)
   (if (null? clauses)
@@ -23,10 +24,12 @@
                 (error "ELSE clause isn't last: COND->IF"
                        clauses))
             (if (cond-clause-with-extension? first)
-                (make-if (cond-predicate-with-extenssion first)
+                (make-if (cond-predicate-with-extension first)
                          ;(sequence->exp (cond-action-with-extension first))
-                         ;((cond-action-with-extension first) ((cond-predicate-with-extenssion first)))
-                         (list (sequence->exp (cond-action-with-extension first)) (cond-predicate-with-extenssion first)) ; building AST, building structure to be evaluate by eval function
+                         ;((cond-action-with-extension first) ((cond-predicate-with-extension first)))
+                         ;(list (sequence->exp (cond-action-with-extension first)) (cond-predicate-with-extension first)) ; building AST, building structure to be evaluate by eval function
+                         (list (cond-recipient-with-extension first) (cond-predicate-with-extension first)) ; both work for (cond ((assoc 'b '((a 1) (b 2))) => (lambda (x) (cadr x))) (else false)) why?
+                         ; the designed language happens to have the same lisp syntax with the designing language, so we place the recipient function into a list, it could evaluate normally. but if we are build this script with another language, it probably would cause an error.
                          (expand-clauses rest))
                 (make-if (cond-predicate first)
                          (sequence->exp (cond-actions first))
