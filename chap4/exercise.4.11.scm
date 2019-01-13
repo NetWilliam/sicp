2898cbf95e6963a1ce00ebfce8fd349d2be76f72
NetWilliam <NWm.LWB@gmail.com>
2019-01-13 17:37:22 +0800

    [mod] exercise.4.11.scm done

--- chap4/exercise.4.11.scm
+++ chap4/exercise.4.11.scm
@@ -4,29 +4,30 @@
 ; scheme concept alist, assoc, assv and assq refer to:
 ; https://www.gnu.org/software/mit-scheme/documentation/mit-scheme-ref/Association-Lists.html#FOOT12
 
-(define (extend-environment vars vals base-env)
-  (if (= (length vars) (length vals))
-      (if (= (length vars) 0)
-          base-env
-          (extend-environment (cdr vars)
-                              (cdr vals)
-                              (cons (cons (car vars)
-                                          (car vals))
-                                    base-env)))
-      (if (< (length vars) (length vals))
-          (error "Too many arguments supplied" vars vals)
-          (error "Too few arguments supplied" vars vals))))
+;(define (extend-environment vars vals base-env)
+;  (if (= (length vars) (length vals))
+;      (if (= (length vars) 0)
+;          base-env
+;          (extend-environment (cdr vars)
+;                              (cdr vals)
+;                              (cons (cons (car vars)
+;                                          (car vals))
+;                                    base-env)))
+;      (if (< (length vars) (length vals))
+;          (error "Too many arguments supplied" vars vals)
+;          (error "Too few arguments supplied" vars vals))))
+
 
 (define (define-variable! var val env)
   (let ((result (assoc var env)))
     (if (not result)
-        (append env (list (cons var val)))
+        (set-cdr! env (append (list (cons var val)) (cdr env)))
         (begin (set-cdr! result val)
                env))))
 
 (define (extend-environment vars vals base-env)
   (if (= (length vars) (length vals))
-      (append (zip vars vals) base-env)
+      (append (map (lambda (x) (cons (car x) (cadr x))) (zip vars vals)) base-env)
       (if (< (length vars) (length vals))
           (error "Too many arguments supplied" vars vals)
           (error "Too few arguments supplied" vars vals))))
