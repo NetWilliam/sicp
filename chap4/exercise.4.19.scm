8c2e756c03618620f20e76218906e3ea3d8c7da4
NetWilliam <NWm.LWB@gmail.com>
2019-01-24 23:21:10 +0800

    [add] add example results

--- chap4/exercise.4.19.scm
+++ chap4/exercise.4.19.scm
@@ -5,3 +5,24 @@
     (define a 5)
     (+ a b))
   (f 10))
+
+; MIT scheme complains:
+;Premature reference to reserved name: a
+;To continue, call RESTART with an option number:
+; (RESTART 1) => Return to read-eval-print level 1.
+;
+;2 error>
+
+; example interpretor give:
+
+;;;; M-Eval input:
+;(let ((a 1))
+;  (define (f x)
+;    (define b (+ a x))
+;    (define a 5)
+;    (+ a b))
+;  (f 10))
+;
+;;;; M-Eval output:
+;16
+
