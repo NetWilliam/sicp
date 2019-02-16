def86c8d118ea14b7b9394a617930f41785e190b
NetWilliam <NWm.LWB@gmail.com>
2019-02-17 06:04:15 +0800

    [add] add d problem

--- chap4/exercise.4.30.scm
+++ chap4/exercise.4.30.scm
@@ -24,3 +24,5 @@
 
 ; the thunk e will not be forced once it's not needed by primitve procedures
 ; so that's the answer to subproblem a, b, and c
+
+; as to question d, the reason a thunk will affect the later value is because there are some set! operations in it, so if we can eliminate the `set!` commands in sequence, we can use the code from text to speed up the evaluation, or we can devise some keywords to help user indicate the side effect of the thunk and thus we can do the right evaluation operation
